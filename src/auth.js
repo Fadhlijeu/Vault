554755c0ffc539f2c1a62614255d71414592c962
Fadhli <fadhlijeu@gmail.com>
2026-07-12 21:23:50 +0700

    update

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,5 @@
 // URL script Google Drive lu.
-const API_URL = "https://script.google.com/macros/s/AKfycbw0Ixyj0HHunDmFRxcn-b_p3Liej7u7shw_QOMakyKmrvxNacwbc_Jr5gpEkRZ7SlrF/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzogCFKEjKq-8Mma70wJ-zAF2tpNS4sfOMe6tEWPEL57VVSfN0YKC0s6ulwq-1qeID2/exec";
 
 export function getApiUrl() {
     return API_URL;
